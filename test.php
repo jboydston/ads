5f67921e69b639562f533300f14a00a34cd529ee
Joe Boydston <jboydston@gmail.com>
2009-10-26 14:45:26 -0700

    rename index, change color to blue

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ href="main.css" />
 -->
 
 <div id="todays_ads_top_bar"
-style="background-color:green;
+style="background-color:blue;
 left:100px;
 top:100px;
 width:190px;
@@ -21,14 +21,12 @@ Today's Advertisers
 $define[random_ad] = array('"1.png"', '"2.png"', '"3.png"', '"4.png"');
 $random_pick = rand(0, count($define[random_ad]) -1);
 $randomad_id = $define[random_ad][$random_pick];
-//$link = "<img src=$randomad_id, width=190/>";
 $link = "<a href=$randomad_id target=_blank><img src=$randomad_id width=190/></a>";
 echo $link;
-//<A HREF="resumepage.html">my resume</A>
 ?>
 <br>
 <div id="todays_ads_bottom_bar"
-style="background-color:green;
+style="background-color:blue;
 left:100px;
 top:100px;
 width:190px;
